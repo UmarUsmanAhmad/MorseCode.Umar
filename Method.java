b6575e8312ec891c87e9777365df087f034a9f1d
Umar <umar.sweden@gmail.com>
2023-03-07 15:19:20 +0100

    MorseCode

--- Method.java
+++ Method.java
@@ -1,36 +1,62 @@
 package UppgiftMorsecode;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Method {
 
 
     //Attributes/Variables
-  public   String[] english = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    public String[] english = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
             "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
             "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
             ",", ".", "?"};
 
-   public String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+    public String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
             ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
             "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
             "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
             "-----", "--..--", ".-.-.-", "..--.."};
 
-    String userResult = "";
+    public String userResult = "";
+
+    public String Method1(String Text1) {
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine().toLowerCase();
+        userInput.split("");
 
 
-    public String Method1(String a) {
-        return ".-";
+
+        for (int j = 0; j < userInput.length(); j++) {
+            for (int i = 0; i < english.length; i++) {
+                if (english[i].equals(String.valueOf(userInput.charAt(j)))) {
+                    userResult += morse[i];
+                }
+            }
+        }
+        System.out.println("Translate: " + userResult);
+
+        return userResult;
     }
 
-    public String Method2(String b) {
+    public String Method2(String Text2) {
+        Scanner scan = new Scanner(System.in);
 
-        return "A";
+        String userInput = scan.nextLine();
+
+        String userResult = Arrays.toString(userInput.split(" "));
+
+        for (int j = 0; j < 1; j++) {
+            for (int i = 0; i < morse.length; i++) {
+
+                if (userInput.equals(morse[i])) {
+                    userResult = english[i];
+                }
+            }
+            System.out.println("Translate: " + userResult);
+        }
+        return userResult;
     }
 
-    public String Method3(String c) {
-
-        return ".--...-.-.";
-    }
 
 }
 
--- TestMorse.java
+++ TestMorse.java
@@ -11,11 +11,11 @@ public class TestMorse {
     @Test
 
     public void TestLettertoMorse() {
-        //Object
+
         Method data = new Method();
         String a = "A";
 
-        String actual = data.Method1(a);
+        String actual = data.Method1("");
 
         String expected = ".-";
         Assertions.assertEquals(expected, actual);
@@ -26,20 +26,22 @@ public class TestMorse {
 
         Method data = new Method();
         String b = ".-";
-        String actual = data.Method2(b);
+        String actual = data.Method2("");
 
         String expected = "A";
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+  /*  @Test
     public void TestWordtoMorse() {
 
         Method data = new Method();
         String c = "ABC";
         String actual = data.Method3(c);
 
-        String expected = ".--...-.-.";
+
         Assertions.assertEquals(expected, actual);
     }
+
+   */
 }
